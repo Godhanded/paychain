d1df659f2949c77e4a2ed9d83d1846210b3c9118
"Godhanded" <"aezeigwe2003@gmail.com">
2022-05-20 01:21:51 -0700

    payment contract

---
+++ contracts/Token.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+/*
+* @author Blockgames Devs
+* @title Cryptopayments token
+*/
+
+/* @notice The owner is the address that deploys the contract*/
+contract CryptoToken is ERC20, Ownable {
+    constructor() ERC20("CryptoToken", "CTK") {}
+
+    /* 
+    * @notice mints specified amount of tokens to an address.
+    * @dev only the owner can call this function.
+    */
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
